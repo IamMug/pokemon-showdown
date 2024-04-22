8b95b6ef9978513e983fa74b19d3f8a6cc5577f9
IamMug <108377633+IamMug@users.noreply.github.com>
2024-04-22 11:21:14 -0230

    bulby test

--- data/pokedex.ts
+++ data/pokedex.ts
@@ -2,9 +2,9 @@ export const Pokedex: {[speciesid: string]: SpeciesData} = {
 	bulbasaur: {
 		num: 1,
 		name: "Bulbasaur",
-		types: ["Grass", "Poison"],
+		types: ["Grass", "Fairy"],
 		genderRatio: {M: 0.875, F: 0.125},
-		baseStats: {hp: 45, atk: 49, def: 49, spa: 65, spd: 65, spe: 45},
+		baseStats: {hp: 145, atk: 49, def: 49, spa: 65, spd: 65, spe: 45},
 		abilities: {0: "Overgrow", H: "Chlorophyll"},
 		heightm: 0.7,
 		weightkg: 6.9,
